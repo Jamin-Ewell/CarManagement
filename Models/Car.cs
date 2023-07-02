04f0c9cf15987c9e384930dd6e8d45a05433bf19
Jamin Ewell <jamin.ewell@charlie24.com>
2023-07-03 00:03:30 +0300

    initial commit

---
+++ Models/Car.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarManagement.Models
+{
+    public class Car
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Make { get; set; }
+
+        [Required]
+        public string Model { get; set; }
+
+        [Required]
+        public int Year { get; set; }
+    }
+}
